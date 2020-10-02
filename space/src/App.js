79bf9337a73db1c61d098db3cdd570f9cb6b299c
Aditya K <aditya.kocherlakota@gmail.com>
2020-10-02 14:00:27 +0530

    adding project

---
+++ space/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+import Space from './components/space/space.js'
+
+
+
+class App extends React.Component {
+  Location = () =>{
+    
+    fetch('https://api.wheretheiss.at/v1/satellites/25544')
+    .then(response=>response.json())
+    .then(data=>{
+        // this.setState({latitude:data.latitude,longitude:data.longitude});
+        let lat = data.latitude;
+        let longi = data.longitude;
+        fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=T8546H6U13P4&format=json&by=position&lat=${lat}&lng=${longi}`)
+        .then(response=>response.json())
+        .then(country=>{
+          if(country.status==="FAILED"){
+            if(this.state.loc!=="over international waters"){
+              this.setState({loc:"over international waters"});
+            }
+          }
+          else{
+            this.setState({loc:country.countryName})
+          }
+          
+        });
+    });
+  }
+
+  
+  constructor(){
+    super();
+    this.state={
+      loc : ""
+    }
+    
+  }
+
+  render(){
+    window.setInterval(this.Location(),10000);
+    console.log(this.state.loc);
+    this.Location();
+    return(
+      <div id="App">
+        <Space/>
+        <h2>{this.state.loc}</h2>
+      </div>
+    );
+
+  }
+
+
+}
+
+// function App() {
+//   return (
+//     <div id="App">
+      
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ space/src/components/space/space.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './spaceStyle.css'
+
+
+
+const Space = () =>{
+	return(
+	<div>
+		<iframe title="1" width="1920" height="1080" src="https://www.youtube.com/embed/XBPjVzSoepo?controls=0&autoplay=1" scrolling="no" 
+		frameBorder="0" allow="accelerometer; autoplay; 
+		clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+		allowFullScreen></iframe>
+	<nav>
+		<div className="listcont">
+			THE INTERNATIONAL SPACE STATION	
+		</div>
+		
+	</nav>
+	<div id="helloWorld">
+	<h1>
+		HELLO WORLD
+	</h1>
+	</div>
+	
+	</div>			
+		
+	);	
+}
+
+export default Space;
