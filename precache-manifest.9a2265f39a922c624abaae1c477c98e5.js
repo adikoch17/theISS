a61c431ff65edbd19cdae58e7671834817c06d08
Aditya K <aditya.kocherlakota@gmail.com>
2020-10-02 14:34:41 +0530

    Updates

---
+++ precache-manifest.9a2265f39a922c624abaae1c477c98e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef6b3b98a52d8e8d3df65dedcb5ae23",
+    "url": "/theISS/index.html"
+  },
+  {
+    "revision": "d836c9e9e65a60597040",
+    "url": "/theISS/static/css/main.99ce4636.chunk.css"
+  },
+  {
+    "revision": "e8485d27baaa8a69763b",
+    "url": "/theISS/static/js/2.5e317440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theISS/static/js/2.5e317440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d836c9e9e65a60597040",
+    "url": "/theISS/static/js/main.5c0aad4b.chunk.js"
+  },
+  {
+    "revision": "52e0968c7566193aa9a1",
+    "url": "/theISS/static/js/runtime-main.26052ed3.js"
+  },
+  {
+    "revision": "5825a4195cfe327d2e0b0dd4439631a8",
+    "url": "/theISS/static/media/Apex Mk3-ExtraLight.5825a419.otf"
+  },
+  {
+    "revision": "434a6fa18ff7a6ae5e8d68ec91597e38",
+    "url": "/theISS/static/media/Apex Mk3-Medium.434a6fa1.otf"
+  }
+]);
